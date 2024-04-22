1f4cba5445dd4bbb6f4e6e88e5263e71aa536c14
JHANINASUAREZ <jhanissyandi@gmail.com>
2024-04-22 18:11:20 -0400

    Registro de ambientes

--- Paginas/Administrador/HomeA.php
+++ Paginas/Administrador/HomeA.php
@@ -24,6 +24,7 @@ $nombreUsuario = $row['nombre'];
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    
 </head>
 
 <body>
@@ -65,12 +66,20 @@ $nombreUsuario = $row['nombre'];
                         <i class="bi bi-house-door-fill fs-4"></i>
                         <span>INICIO</span>
                     </a>
-                </li>
+                    </li>
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#RegistrarA" aria-expanded="false" aria-controls="RegistrarA" style="text-decoration: none;">
+                    <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#RegistrarA" aria-expanded="false" aria-controls="Registrar_ambiente" style="text-decoration: none;">
                     <img width="25" height="25" src="https://img.icons8.com/ios-filled/50/plus-2-math.png" alt="plus-2-math" style="filter: invert(100%);margin-right: 10px;"/>
                         <span>REGISTRAR AMBIENTE</span>
                     </a>
+                    <ul id="RegistrarA" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                        <li class="sidebar-item">
+                       <!-- <a href="registrar_ambiente.php" class="sidebar-link" style="text-decoration: none;">REGISTRO DE AMBIENTE</a>-->
+                       <!-- Enlace para abrir el modal -->
+                    <a href="#" class="sidebar-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" style="text-decoration: none;">Registrar Ambiente</a>
+
+                        </li> 
+                    </ul>
                     <ul id="RegistrarA" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                         <li class="sidebar-item">
                             <a href="#" class="sidebar-link" style="text-decoration: none;">LISTA DE AMBIENTES REGISTRADOS</a>
@@ -148,6 +157,73 @@ $nombreUsuario = $row['nombre'];
         </div>
     </div>
 
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+    <div class="modal-dialog d-flex justify-content-center">
+        <div class="modal-content w-75">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel2">Registrar Ambiente</h5>
+                <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-4">
+            <form action="guardar_ambiente.php" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="nombre">Nombre del Ambiente</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="capacidad">Capacidad Máxima</label>
+                        <input type="number" class="form-control" id="capacidad" name="capacidad" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="ubicacion">Ubicación</label>
+                        <input type="text" class="form-control" id="ubicacion" name="ubicacion" required>
+                        </div>
+                    <div class="form-group">
+                        <label for="periodo">Periodo de Examen</label>
+                        <select class="form-control" id="periodo" name="periodo" required>
+                            <option value="primer parcial">Primer Parcial</option>
+                            <option value="segundo parcial">Segundo Parcial</option>
+                            <option value="tercer parcial">Examen final</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="fechaInicio">Fecha de inicio</label>
+                        <input type="date" class="form-control" id="fechaInicio" name="fechaInicio" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="fechaFin">Fecha de fin</label>
+                        <input type="date" class="form-control" id="fechaFin" name="fechaFin" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="horario">Horario Disponible</label>
+                        <select class="form-control" id="horario" name="horario" required>
+                            <option value="06:45">06:45</option>
+                            <option value="08:15">08:15</option>
+                            <option value="tercero">09:45</option>
+                            <option value="cuarto">11:15</option>
+                            <option value="quinto">12:45</option>
+                            <option value="sexto">14:15</option>
+                            <option value="septimo">15:45</option>
+                            <option value="octavo">17:15</option>
+                            <option value="noveno">18:45</option>
+                            <option value="decimo">20:15</option>
+
+                        </select>
+                        
+                    </div>
+                    <div class="form-group">
+                        <label for="imagen">Seleccionar Imagen</label>
+                        <input type="file" class="form-control-file" id="imagen" name="imagen">
+                    </div>
+                    <button type="submit" class="btn btn-success">Guardar</button>
+                    <button type="reset" class="btn btn-danger">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="../../js/MenuLateral.js"></script>
